b024cddc18fcca3db7d88e324a4a15ed27ff57f9
Brciks666 <tconckoff82@gmail.com>
2022-02-04 18:28:39 +0300

    add eslint config

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./authRouter')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/auth", authRouter)
+app.use(express.json());
+app.use("/auth", authRouter);
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://qwerty11:qwerty123@cluster0.ulses.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
+	try {
+		await mongoose.connect(
+			`mongodb+srv://qwerty11:qwerty123@cluster0.ulses.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+		);
+		app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
+};
 
-start()
-
+start();
